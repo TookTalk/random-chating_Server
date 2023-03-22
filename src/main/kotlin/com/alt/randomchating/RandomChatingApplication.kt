b03999adeaa6c13cdc88310c5c9ecd83a8b177ae
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-03-23 08:39:34 +0900

    commit

---
+++ src/main/kotlin/com/alt/randomchating/RandomChatingApplication.kt
@@ -0,0 +1,11 @@
+package com.alt.randomchating
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RandomChatingApplication
+
+fun main(args: Array<String>) {
+    runApplication<RandomChatingApplication>(*args)
+}
---
+++ src/test/kotlin/com/alt/randomchating/RandomChatingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alt.randomchating
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RandomChatingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
