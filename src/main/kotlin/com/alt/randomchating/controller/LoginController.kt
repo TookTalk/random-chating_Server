c699b30662302634de1153a40d1d00a3ff4b2eba
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-03-24 23:39:40 +0900

    commit

--- src/main/kotlin/com/alt/randomchating/controller/LoginController.kt
+++ src/main/kotlin/com/alt/randomchating/controller/LoginController.kt
@@ -1,14 +1,14 @@
 package com.alt.randomchating.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
-@RequestMapping("/api")
 @Controller
 class LoginController {
-@PostMapping("value = \"/user\"")
+@RequestMapping("/login")
     fun login(): String{
-        return "100"
+        return "Hello"
     }
 }
